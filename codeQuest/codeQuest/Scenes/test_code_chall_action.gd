64a855ee7f43860efa463dc64a5ac6338ad67444
Carlos Vasquez <vasqueca@oregonstate.edu>
2025-02-09 17:14:52 -0800

    removed all of the cache files in order to make pushing changes easier for everyone

--- codeQuest/codeQuest/Scenes/test_code_chall_action.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-
-@export var dialogue_resource: DialogueResource
-@export var dialogue_start: String = "start"
-
-
-func action() -> void:
-	get_tree().change_scene_to_file("res://Scenes/TestCodeChallange/TestCodeChallenge.tscn")
